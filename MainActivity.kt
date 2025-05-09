4f94926a095359de2f9aeb2c00f341c0754b6f92
Rudraraj Mali <rudrarajmali93@gmail.com>
2025-05-09 06:58:30 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,66 +2,35 @@ package com.example.call
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.activity.result.contract.ActivityResultContracts
 
-
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            SimpleCameraOpener()
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyApp() {
 
-    MaterialTheme{
-        var text by remember { mutableStateOf("") }
+    @Composable
+    fun SimpleCameraOpener() {
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+        // Do nothing with the result
+    }
 
+    Button(onClick = { launcher.launch(null) }) {
+        Text("Open Camera")
+    }
+}
 
-        Column {
-            Text(
-                modifier = Modifier.fillMaxWidth().background(color = Color.Red)
-                 ,
-                text = "Welcome",
-                fontSize = 40.sp,
-
-                )
-            Spacer(modifier = Modifier.height(20.dp))
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Password") }
-            )
-            val ite = listOf("Apple", "Banana", "Cherry")
-
-            LazyColumn {
-                items(ite) { item ->
-                    Text(text = item)
-                }
-            }
-
-        }
-
-
-            }
-        }
 
 
